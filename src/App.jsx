3b68f9bcb537119c49daaf1208e0b1957e0eec9f
Emre Cagri Sabanci <emre.sabanci@pitcher.com>
2024-03-02 22:22:18 +0300

    chore: installing tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import { Link, BrowserRouter, Routes, Route } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import SearchParams from './SearchParams'
-import Details from './Details'
-import { useState } from 'react'
-import AdoptedPetContext from './AdoptedPetContext'
+import { createRoot } from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { useState } from "react";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -13,27 +13,34 @@ const queryClient = new QueryClient({
       cacheTime: Infinity,
     },
   },
-})
+});
 
 const App = () => {
-  const adoptedPetHook = useState(null)
+  const adoptedPetHook = useState(null);
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <AdoptedPetContext.Provider value={adoptedPetHook}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />}></Route>
-            <Route path="/" element={<SearchParams />}></Route>
-          </Routes>
-        </AdoptedPetContext.Provider>
-      </QueryClientProvider>
-    </BrowserRouter>
-  )
-}
+    <div
+      className="m-0 p-0"
+      style={{
+        background: "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg",
+      }}
+    >
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <AdoptedPetContext.Provider value={adoptedPetHook}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />}></Route>
+              <Route path="/" element={<SearchParams />}></Route>
+            </Routes>
+          </AdoptedPetContext.Provider>
+        </QueryClientProvider>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<App />)
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
